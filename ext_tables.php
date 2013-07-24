3b4b1d20c2a7401b57dea9942f1b23fd4d3c8ae6
Matthias Gugel <mail@matthias-gugel.de>
2013-07-24 18:11:10 +0200

    [BUGFIX] remove wrong namespace prefix prior to $_EXTKEY

--- ext_tables.php
+++ ext_tables.php
@@ -4,11 +4,11 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'MFG.' . $_EXTKEY,
+	$_EXTKEY,
 	'Dwzlist',
 	'DWZ list'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('MFG.' . $_EXTKEY, 'Configuration/TypoScript', 'DWZ list');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'DWZ list');
 
 ?>
